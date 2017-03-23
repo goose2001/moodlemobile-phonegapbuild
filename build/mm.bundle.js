fbc73e920be79646adafe80843a91289c007718a
goose2001 <dan@appliededucation.edu.au>
2017-03-23 09:52:23 +0800

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -37524,6 +37524,6 @@ angular.module('mm.core')
     "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-	"siteurl": "http://online.appliededucation.edu.au"
+	"siteurl": "https://online.appliededucation.edu.au"
 }
 );
